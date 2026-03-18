5ac1ef07953488a1916fe0c8e96973a35adac41d
vivek kumar <43455188+yaduvanshi777@users.noreply.github.com>
2026-03-18 09:31:57 +0530

    runnable

---
+++ langchain runable/runnable_branch.py
@@ -0,0 +1,35 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableBranch, RunnableLambda
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Summarize the following text \n {text}',
+    input_variables=['text']
+)
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7
+)
+
+parser = StrOutputParser()
+
+report_gen_chain = prompt1 | model | parser
+
+branch_chain = RunnableBranch(
+    (lambda x: len(x.split())>300, prompt2 | model | parser),
+    RunnablePassthrough()
+)
+
+final_chain = RunnableSequence(report_gen_chain, branch_chain)
+
+print(final_chain.invoke({'topic':'llm or rag '}))
--- langchain runable/runnable_lambda.py
+++ langchain runable/runnable_lambda.py
@@ -2,11 +2,14 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough
+from langchain_core.runnables import RunnableSequence, RunnableLambda, RunnablePassthrough, RunnableParallel
 
 load_dotenv()
 
-prompt1 = PromptTemplate(
+def word_count(text):
+    return len(text.split())
+
+prompt = PromptTemplate(
     template='Write a joke about {topic}',
     input_variables=['topic']
 )
@@ -18,18 +21,17 @@ model = ChatGoogleGenerativeAI(
 
 parser = StrOutputParser()
 
-prompt2 = PromptTemplate(
-    template='Explain the following joke - {text}',
-    input_variables=['text']
-)
-
-joke_gen_chain = RunnableSequence(prompt1, model, parser)
+joke_gen_chain = RunnableSequence(prompt, model, parser)
 
 parallel_chain = RunnableParallel({
     'joke': RunnablePassthrough(),
-    'explanation': RunnableSequence(prompt2, model, parser)
+    'word_count': RunnableLambda(word_count)
 })
 
 final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
 
-print(final_chain.invoke({'topic':'cricket'}))
+result = final_chain.invoke({'topic':'AI'})
+
+final_result = """{} \n word count - {}""".format(result['joke'], result['word_count'])
+
+print(final_result)
